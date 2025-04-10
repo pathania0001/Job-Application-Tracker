fcfb9e5dfc5d4c545802efa5798c7c65c173d79a
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2025-04-10 21:34:12 +0530

    cors issues solve

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// app.js
+
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.js";
@@ -11,9 +11,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || "*", 
+  origin: "http://localhost:5173",
   credentials: true
 }));
+
 
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true, limit: "500mb" }));
